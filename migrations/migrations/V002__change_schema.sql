93dc24b1798dc0e26c20e319081490e9d0efce4e
Paponova Viktoriya <paponova.vika@gmail.com>
2025-03-17 09:35:12 +0200

    change schema

---
+++ migrations/migrations/V002__change_schema.sql
@@ -0,0 +1,6 @@
+alter table product add column     price      double precision;
+drop table product_info;
+alter table orders add column     date_created date default current_date;
+drop table orders_date;
+alter table order_product add foreign key (product_id) references product(id),
+add foreign key (order_id) references orders(id);
